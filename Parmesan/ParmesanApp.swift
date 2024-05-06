7b206bb292fcf1f9b5245c524b19a0ecd395aa32
su <su@sususu.su>
2024-05-06 13:20:20 +0800

    Initial Commit

---
+++ Parmesan/ParmesanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParmesanApp.swift
+//  Parmesan
+//
+//  Created by 郭粟閣 on 2024/5/6.
+//
+
+import SwiftUI
+
+@main
+struct ParmesanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
